b092da005c60cf5b3b361a74134917365fc5a2fb
Elias Mawa <elias@mawa.dev>
2020-06-30 21:56:04 -0600

    chore: improve memory management

--- src/core/game.c
+++ src/core/game.c
@@ -1,6 +1,7 @@
 #include "game.h"
 #include "../ecs.h"
 
+#include <stdlib.h>
 #include <SDL2/SDL_timer.h>
 
 /*****************************************************
@@ -50,51 +51,47 @@ bool sorbet__init(SORBET_T* sorbet, SORBET_OPTIONS_T *options)
         printf("SDL2 could not initalize! Error: %s\n", SDL_GetError());
 		return false;
     }
-    else
+
+    sorbet->window = SDL_CreateWindow(options->title,
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		options->width, options->height, options->flags);
+    if(!sorbet->window)
     {
-        sorbet->window = SDL_CreateWindow(options->title,
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-			options->width, options->height, options->flags);
-        if(sorbet->window)
-        {
-			sorbet->renderer = SDL_CreateRenderer(sorbet->window, -1,
-				SDL_RENDERER_ACCELERATED);
-			if(!sorbet->renderer)
-			{
-				printf("Renderer could not initalize! Error: %s\n",
-					SDL_GetError());
-				return false;
-			}
+        printf("Window could not be created! Error: %s\n", SDL_GetError());
+		return false;
+	}
 
-			sorbet->surface = SDL_GetWindowSurface(sorbet->window);
-			if(!sorbet->surface)
-			{
-				printf("Surface could not initalize! Error: %s\n",
-					SDL_GetError());
-				return false;
-			}
-			
-			int img_flags = IMG_INIT_PNG;
-			if(!(IMG_Init(img_flags) & img_flags))
-			{
-				printf("SDL_image could not initialize! Error: %s\n",
-					IMG_GetError());
-				return false;
-			}
+	sorbet->renderer = SDL_CreateRenderer(sorbet->window, -1,
+		SDL_RENDERER_ACCELERATED);
+	if(!sorbet->renderer)
+	{
+		printf("Renderer could not initalize! Error: %s\n",
+			SDL_GetError());
+		return false;
+	}
 
-			if(TTF_Init() == -1)
-			{
-				printf("SDL_ttf could not initialize! Error: %s\n",
-					TTF_GetError());
-				return false;
-			}
-		}
-		else
-		{
-            printf("Window could not be created! Error: %s\n",SDL_GetError());
-			return false;
-		}
-    }
+	sorbet->surface = SDL_GetWindowSurface(sorbet->window);
+	if(!sorbet->surface)
+	{
+		printf("Surface could not initalize! Error: %s\n",
+			SDL_GetError());
+		return false;
+	}
+	
+	int img_flags = IMG_INIT_PNG;
+	if(!(IMG_Init(img_flags) & img_flags))
+	{
+		printf("SDL_image could not initialize! Error: %s\n",
+			IMG_GetError());
+		return false;
+	}
+
+	if(TTF_Init() == -1)
+	{
+		printf("SDL_ttf could not initialize! Error: %s\n",
+			TTF_GetError());
+		return false;
+	}
 
 	sorbet->options = options;
 	return true;
@@ -102,6 +99,11 @@ bool sorbet__init(SORBET_T* sorbet, SORBET_OPTIONS_T *options)
 
 void sorbet__tick(SORBET_T* sorbet, SDL_Event* event, SORBET_LENGTH_T delta)
 {
+	if(!sorbet || !sorbet->options || !sorbet->renderer)
+	{
+		return;
+	}
+
 	COLLECTION_T* collection = sorbet->options->collection;
 
 	SDL_SetRenderDrawColor(sorbet->renderer, 0x00, 0x00, 0x00, 0xFF);
@@ -117,23 +119,34 @@ void sorbet__tick(SORBET_T* sorbet, SDL_Event* event, SORBET_LENGTH_T delta)
 			default:
 				break;
 		}
+	}
 
+	if(collection)
+	{
 		collection__tick(sorbet, collection, event, delta);
-		sorbet__custom_tick ? sorbet__custom_tick(sorbet, event, delta) : NULL;
+
+		for(size_t i = 0; i < collection->entity_count; i++)
+		{
+			ENTITY_T* entity = collection->entity_list->data[i];
+			if(entity && entity->update)
+			{
+				entity->update(sorbet, entity->payload, delta);
+			}
+		}
+
+		for(size_t i = 0; i < collection->entity_count; i++)
+		{
+			ENTITY_T* entity = collection->entity_list->data[i];
+			if(entity && entity->render)
+			{
+				entity->render(sorbet, entity->payload, delta);
+			}
+		}
 	}
 
-	for(size_t i = 0; i<collection->entity_count; i++)
+	if(sorbet__custom_tick)
 	{
-		ENTITY_T* entity = collection->entity_list->data[i];
-		if(entity->update)
-			entity->update(sorbet, entity->payload, delta);
-	}
-
-	for(size_t i = 0; i<collection->entity_count; i++)
-	{
-		ENTITY_T* entity = collection->entity_list->data[i];
-		if(entity->render)
-			entity->render(sorbet, entity->payload, delta);
+		sorbet__custom_tick(sorbet, event, delta);
 	}
 
 	SDL_RenderPresent(sorbet->renderer);
@@ -145,6 +158,12 @@ void sorbet__run(SORBET_T* sorbet)
 	SORBET_LENGTH_T TARGET_TICKS_PER_FRAME = 0;
 	SORBET_LENGTH_T TIME = 0, LAST = 0, DELTA = 0;
 
+	if(!sorbet->options || sorbet->options->framerate <= 0)
+	{
+		printf("Invalid framerate or options.\n");
+		return;
+	}
+
 	while (!sorbet->quit)
 	{
 		SORBET_LENGTH_T FRAMERATE = sorbet->options->framerate;
@@ -154,7 +173,7 @@ void sorbet__run(SORBET_T* sorbet)
 
 		TIME = SDL_GetTicks();
 		DELTA = (TIME - LAST) > (FRAMERATE * 3)
-			? (FRAMERATE * 3) / 1000 : (TIME - LAST) / 1000;
+			? (FRAMERATE * 3) / 1000.0 : (TIME - LAST) / 1000.0;
 
 		sorbet__tick(sorbet, &event, DELTA);
 
@@ -166,21 +185,26 @@ void sorbet__run(SORBET_T* sorbet)
 
 void sorbet__exit(SORBET_T* sorbet)
 {
-	// deallocate surface
-	SDL_FreeSurface(sorbet->surface);
-	sorbet->surface = NULL;
-
 	// destroy renderer
-	SDL_DestroyRenderer(sorbet->renderer);
-	sorbet->renderer = NULL;
+	if(sorbet->renderer)
+	{
+		SDL_DestroyRenderer(sorbet->renderer);
+		sorbet->renderer = NULL;
+	}
 
-	// destroy window
-	SDL_DestroyWindow(sorbet->window);
-	sorbet->window = NULL;
+	// destroy window (also frees the window surface)
+	if(sorbet->window)
+	{
+		SDL_DestroyWindow(sorbet->window);
+		sorbet->window = NULL;
+	}
+	sorbet->surface = NULL;
 
     TTF_Quit();
     IMG_Quit();
  
 	// quit SDL subsystems
 	SDL_Quit();
+
+	free(sorbet);
 } // void sorbet__exit()
--- src/core/game.h
+++ src/core/game.h
@@ -29,23 +29,25 @@ typedef struct SORBET_OPTIONS SORBET_OPTIONS_T;
  * TYPE IMPLEMENTATIONS
  *****************************************************/
 
-typedef struct SORBET {
-	SORBET_OPTIONS_T* options; // options for sorbet object
-	SDL_Window* window; // SDL2 window refrence
-	SDL_Surface* surface; // SDL2 surface refrence
-	SDL_Renderer* renderer; // SDL2 renderer refrence
-	bool quit; // object quit flag
+typedef struct SORBET
+{
+  SORBET_OPTIONS_T *options; // options for sorbet object
+  SDL_Window *window;        // SDL2 window refrence
+  SDL_Surface *surface;      // SDL2 surface refrence
+  SDL_Renderer *renderer;    // SDL2 renderer refrence
+  bool quit;                 // object quit flag
 } SORBET_T;
 
-typedef struct SORBET_OPTIONS {
-	COLLECTION_T* collection;
-	char* title; // window title.
-	short width;  // window width.
-	short height; // window height.
-	short framerate; // target frames per second.
-	uint32_t flags; // SDL2 init flags.
-	bool debug; // debugging & development flag.
-	bool log; // logging system flag.
+typedef struct SORBET_OPTIONS
+{
+  COLLECTION_T *collection;
+  char *title;     // window title.
+  short width;     // window width.
+  short height;    // window height.
+  short framerate; // target frames per second.
+  uint32_t flags;  // SDL2 init flags.
+  bool debug;      // debugging & development flag.
+  bool log;        // logging system flag.
 } SORBET_OPTIONS_T;
 
 /*****************************************************
@@ -56,32 +58,32 @@ typedef struct SORBET_OPTIONS {
  * generate a blank sorbet object.
  * @returns an empty sorbet object.
  **/
-SORBET_T* sorbet__create();
+SORBET_T *sorbet__create();
 
 /**
  * initialize sorbet and all it's subsytems.
  * @param sorbet sorbet object to be initialized.
  * @param options options to apply to sorbet object.
  **/
-bool sorbet__init(SORBET_T* sorbet, SORBET_OPTIONS_T* options);
+bool sorbet__init(SORBET_T *sorbet, SORBET_OPTIONS_T *options);
 
 /**
  * compute a tick (frame).
  * @param sorbet sorbet object to compute.
  */
-void sorbet__tick(SORBET_T* sorbet, SDL_Event* event, SORBET_LENGTH_T delta);
+void sorbet__tick(SORBET_T *sorbet, SDL_Event *event, SORBET_LENGTH_T delta);
 
 /**
  * compute a tick (frame).
  * @param sorbet sorbet object to compute.
  */
-void sorbet__run(SORBET_T* sorbet);
+void sorbet__run(SORBET_T *sorbet);
 
 /**
  * exit sorbet and uninatialize all subsystems.
  * @param sorbet sorbet object to compute.
  */
-void sorbet__exit(SORBET_T* sorbet);
+void sorbet__exit(SORBET_T *sorbet);
 
 /*****************************************************
  * FUNCTION IMPLEMENTATIONS
@@ -91,7 +93,7 @@ void sorbet__exit(SORBET_T* sorbet);
  * a custom tick function to run during game loop. (IMPLEMENTED BY DEVELOPER)
  * @param sorbet sorbet object to compute.
  */
-extern void (*sorbet__custom_tick)(SORBET_T* sorbet,
-	SDL_Event* event, SORBET_LENGTH_T delta);
+extern void (*sorbet__custom_tick)(SORBET_T *sorbet,
+                                   SDL_Event *event, SORBET_LENGTH_T delta);
 
 #endif
